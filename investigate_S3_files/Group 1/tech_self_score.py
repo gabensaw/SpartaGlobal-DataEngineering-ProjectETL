e2b43078a4f563524fdbe9ac6df090a8aa710b0e
Sameer Mesiah <sameer-mesiah@hotmail.com>
2022-08-15 17:36:55 +0100

    Edited tech_self_score function

--- investigate_S3_files/Group 1/tech_self_score.py
+++ investigate_S3_files/Group 1/tech_self_score.py
@@ -1,27 +1,14 @@
 import csv
 
-test_data = [{"tech_self_score": {"C#": 6, "Java": 5, "R": 2, "JavaScript": 2}},
-             {"tech_self_score": {"C": 6, "Java": 5, "Ruby": 2, "JavaScript": 2}}]
+test_data = {"tech_self_score": {"C#": 6, "Java": 5, "R": 2, "JavaScript": 2}}
 
 
-def create_tech_self_score_csv(data):
-    fields_existing = set({})
-    for candidate in test_data:
-        for field_existing in candidate["tech_self_score"].keys():
-            fields_existing.add(field_existing)
-
-    with open('tech_self_score', 'w') as f:
-        writer = csv.writer(f)
-        writer.writerow(list(fields_existing))
-
-        for candidate in test_data:
-            row = []
-            for field_existing in fields_existing:
-                if field_existing in candidate["tech_self_score"].keys():
-                    row.append(candidate["tech_self_score"][field_existing])
-                else:
-                    row.append(0)
-            writer.writerow(row)
+def create_tech_self_score_list(cand_id, scores):
+    row = [cand_id]
+    for skill in scores["tech_self_score"].keys():
+        row.append(skill)
+        row.append(scores["tech_self_score"][skill])
+    return row
 
 
-create_tech_self_score_csv(test_data)
+create_tech_self_score_list(1, test_data)
