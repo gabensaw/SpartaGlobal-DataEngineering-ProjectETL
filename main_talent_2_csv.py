d18523f78742a3e3cc7488f2e87f14efa641ba09
Ali Shaheed <99894982+AliShaheed@users.noreply.github.com>
2022-08-15 15:54:44 +0100

    Added functions to extract main talent fields

---
+++ main_talent_2_csv.py
@@ -0,0 +1,50 @@
+import json
+import boto3
+import csv
+
+# connect to AWS s3
+s3_resource = boto3.resource('s3')
+bucket = s3_resource.Bucket('data-eng-31-final-project')
+objects = bucket.objects.filter(Prefix='Talent')
+
+
+# Takes in a dictionary, extracts fields and returns a list
+def get_fields(id, content):
+    return [id,
+            content["name"],
+            content["date"],
+            content["self_development"],
+            content["geo_flex"],
+            content["financial_support_self"],
+            content["result"],
+            content["course_interest"]]
+
+
+# write list of lists to csv
+def write_to_csv(list_of_lists, filename="main_talent.csv"):
+    with open(filename, "a+", newline='') as csv_file1:
+        csv_writer = csv.writer(csv_file1)
+        csv_writer.writerow(['id',
+                             'name',
+                             'date',
+                             'self_development',
+                             'geo_flex',
+                             'financial_support_self',
+                             'result',
+                             'course_interest'])
+        csv_writer.writerows(list_of_lists)
+
+
+# extract data from json, writes to csv
+def extract(talent_object):
+    main_talent = []
+    for uid, obj in enumerate(talent_object, 1):
+        if obj.key.endswith('.json'):
+            file_content = obj.get()['Body'].read().decode('utf-8')
+            json_content = json.loads(file_content)
+            person = get_fields(uid, json_content)
+            main_talent.append(person)
+    write_to_csv(main_talent)
+
+
+extract(objects)
